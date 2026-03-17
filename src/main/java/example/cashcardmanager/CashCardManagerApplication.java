b2d39132124438705c566c0461bbac02356a9582
Andrew De La Torre <drewdlt@yahoo.com>
2026-03-17 00:03:46 -0700

    initial commit

---
+++ src/main/java/example/cashcardmanager/CashCardManagerApplication.java
@@ -0,0 +1,13 @@
+package example.cashcardmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashCardManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashCardManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/cashcardmanager/CashCardManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package example.cashcardmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCardManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
